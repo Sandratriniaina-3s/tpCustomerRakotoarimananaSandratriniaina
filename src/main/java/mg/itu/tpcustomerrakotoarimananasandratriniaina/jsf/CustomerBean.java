5a5dfdf782bb9aaa9336f11b5e575702ff4c7be5
Sandratra <3s.sandratra@gmail.com>
2024-03-21 09:42:47 +0300

    Ajout ConfigJSF, CustomerBean, customerList et modification du fichier web.xml

---
+++ src/main/java/mg/itu/tpcustomerrakotoarimananasandratriniaina/jsf/CustomerBean.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.tpcustomerrakotoarimananasandratriniaina.jsf;
+
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import java.io.Serializable;
+import java.util.List;
+import mg.itu.tpcustomerrakotoarimananasandratriniaina.entity.Customer;
+import mg.itu.tpcustomerrakotoarimananasandratriniaina.service.CustomerManager;
+
+/**
+ * Backing bean pour la page JSF customerList
+ *
+ * @author Sandratriniaina
+ */
+
+@Named(value = "customerBean")
+@ViewScoped
+public class CustomerBean implements Serializable {
+
+    @Inject
+    private CustomerManager customerManager;
+
+    private List<Customer> customerList;
+
+    public CustomerBean() {
+    }
+
+    /**
+     * Retourne la liste des clients pour affichage dans une DataTable.
+     */
+    public List<Customer> getCustomers() {
+        if (customerList == null) {
+            customerList = customerManager.getAllCustomers();
+        }
+        return customerList;
+    }
+
+}
